f128dce19e3a6d73646d93c4f13bbb19b82e8456
ico277 <enfrico123@gmail.com>
2024-07-20 04:01:58 +0200

    first couple things

---
+++ src/lang.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define RESET "\x1b[0m"
+
+#define KEY "\x1b[38;5;213m%s" RESET
+
+#define INFO_FORMAT KEY SEPERATOR VALUE 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+#include <cstdio>
+#include <fstream>
 #include <iostream>
 
+using std::ifstream;
+
+using std::string;
+
+string get_os() {
+    string os;
+
+    string line;
+    ifstream file("/etc/os-release");
+    while (getline(file, line)) {
+        // TODO filter name
+    }
+
+    return os;
+}
+
+
 int main() {
-    std::cout << "nya\n";
+    std::cout << get_os() << "\n";
 }
